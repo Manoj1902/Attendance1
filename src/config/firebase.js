d8fca2f12e5b9a5633e856508fcf8ee0b03c3d23
manoj1902 <raj.manoj34@gmail.com>
2024-06-01 18:00:27 +0530

    inserting into database

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBkDzC_qsLYmIsBHEbjdwM9R9sW_u9iPNk",
   authDomain: "attendance-f38f8.firebaseapp.com",
@@ -20,6 +16,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
-export const auth = getAuth(app)
+// Initialize Firebase Auth with persistence
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
--- src/screens/InsertEmployeeScreen.jsx
+++ src/screens/InsertEmployeeScreen.jsx
@@ -1,5 +1,5 @@
-import { Dimensions, SafeAreaView, StyleSheet, Text, ToastAndroid, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import { Alert, Dimensions, SafeAreaView, StyleSheet, Text, TextInput, ToastAndroid, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons'
 import { theme } from '../theme'
 import { version } from '../../package.json'
@@ -7,7 +7,13 @@ import { version } from '../../package.json'
 
 
 var { width, height } = Dimensions.get('window')
-const InsertEmployeeScreen = () => {
+const InsertEmployeeScreen = ({ navigation }) => {
+
+    const [hidePassword, setHidePassword] = useState(true)
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     const appVersion = () => {
         ToastAndroid.showWithGravity(
@@ -16,6 +22,48 @@ const InsertEmployeeScreen = () => {
             ToastAndroid.CENTER,
         );
     };
+
+    const handleInsert = () => {
+
+        let uName = name
+        let uEmail = email
+        let uPassword = password
+
+        if (uName.length == 0 || uEmail == 0 || uPassword == 0) {
+            Alert.alert("Required Field is Missing");
+        } else {
+            var InsertURL = 'http://192.168.137.1/api/insert.php'
+
+            var headers = {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+
+            var Data = {
+                Name: uName,
+                Email: uEmail,
+                Password: uPassword
+            }
+
+            fetch(InsertURL, {
+                method: 'POST',
+                headers: headers,
+                body: JSON.stringify(Data)
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    Alert.alert(response[0].Message)
+                })
+                .catch((error) => {
+                    Alert.alert('Error: ' + error)
+                })
+        }
+
+        setName('')
+        setEmail('')
+        setPassword('')
+
+    }
 
     return (
         <View style={styles.container}>
@@ -35,7 +83,40 @@ const InsertEmployeeScreen = () => {
             </SafeAreaView>
 
             <View>
-
+                <View style={styles.input}>
+                    <View>
+                        <Text style={styles.inputTitle}>Name</Text>
+                        <View style={styles.inputContainer}>
+                            <Icon name='person-sharp' padding={8} size={20} color={theme.text} style={{ borderRadius: 8 }} />
+                            <TextInput style={{ width: width * 0.83, color: theme.text, borderRadius: 8 }} placeholderTextColor={"#b0b0b0"} placeholder='Name' value={name} onChangeText={value => setName(value)} />
+                        </View>
+                    </View>
+                    <View>
+                        <Text style={styles.inputTitle}>Email</Text>
+                        <View style={styles.inputContainer}>
+                            <Icon name='at-sharp' padding={8} size={20} color={theme.text} style={{ borderRadius: 8 }} />
+                            <TextInput keyboardType='email-address' style={{ width: width * 0.83, color: theme.text, borderRadius: 8 }} placeholderTextColor={"#b0b0b0"} placeholder='Email' value={email} onChangeText={value => setEmail(value)} />
+                        </View>
+                    </View>
+                    <View>
+                        <Text style={styles.inputTitle}>Password</Text>
+                        <View style={styles.inputContainer}>
+                            <Icon name='lock-closed' padding={8} size={20} color={theme.text} style={{ borderRadius: 8 }} />
+                            <TextInput style={{ width: width * 0.75, color: theme.text, borderRadius: 8 }} placeholderTextColor={"#b0b0b0"} placeholder='Password' secureTextEntry={hidePassword} value={password} onChangeText={value => setPassword(value)} />
+                            <TouchableOpacity onPress={() => setHidePassword(!hidePassword)}>
+                                {
+                                    hidePassword ? <Icon name='eye' padding={8} size={20} color={theme.text} />
+                                        :
+                                        <Icon name='eye-outline' padding={8} size={20} color={theme.text} />}
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                    <TouchableOpacity style={styles.insertBtn} onPress={handleInsert}>
+                        <Text style={styles.insertBtnText}>
+                            Insert
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </View>
     )
@@ -65,5 +146,38 @@ const styles = StyleSheet.create({
         fontSize: 26,
         fontWeight: 'bold',
         color: theme.text
+    },
+    input: {
+        // backgroundColor: 'red',
+        marginTop: 28,
+        padding: 8,
+        gap: 30
+    },
+    inputTitle: {
+        paddingHorizontal: 8,
+        marginBottom: 4,
+        color: theme.text
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        // backgroundColor: 'blue',
+        borderRadius: 10,
+        borderWidth: 1.5,
+        borderColor: '#5e5e5e'
+    },
+    insertBtn: {
+        padding: 10,
+        backgroundColor: theme.themeColor,
+        width: 155,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderRadius: 10
+    },
+    insertBtnText: {
+        fontSize: 18,
+        fontWeight: 'bold',
     },
 })
